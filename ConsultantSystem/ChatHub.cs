3d699d6fbaf229a7d1cddcd3b753af74bbea7e1d
congphuong1703 <51237215+congphuong1703@users.noreply.github.com>
2021-11-09 09:26:45 +0700

    12312#

---
+++ ConsultantSystem/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR;
+
+namespace ConsultantSystem
+{
+    public class ChatHub
+    {
+        public void Send(string name, string message)
+        {
+            var user = name;
+            if (string.IsNullOrEmpty(user))
+                user = "Anonymous";
+            string format = "HH:mm:ss";
+            DateTime now = DateTime.Now;
+            string s = now.ToString(format);
+            user += "\n" + s;
+            Clients.All.addNewMessageToPage(user, message);
+        }
+
+        public void Post(string name, string message)
+        {
+            var user = name;
+            if (string.IsNullOrEmpty(user))
+                user = "Anonymous";
+            Clients.All.addNewMessageToPage(user, message);
+        }
+    }
+}
